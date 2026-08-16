fcf2d4503c1ed0083815ee036f29112e6c2c8aaf
盧信宏 <ryan.lu0228@gmail.com>
2026-08-16 20:28:32 +0800

    更新網站 2026-08-16 20:28

--- images/products/catalog.js
+++ images/products/catalog.js
@@ -3,6 +3,46 @@ window.PRODUCT_CATALOG = {
     "version": 1,
     "products": [
         {
+            "updatedLabel": "2026-08-16｜新品展示",
+            "name": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液",
+            "description": "葡萄牙原裝進口Orgie小紅瓶2代（30ml），採用植物精粹與紅蘋果香氣調配，內含丁香花油、薄荷葉油萃取與維生素E成分。質地溫和潤滑且易於清水沖洗，通過皮膚耐受性與PH值等多項品質測試，提供便利安心的日常個人護理體驗。",
+            "footnote": "實際庫存、售價與包裝版本以門市現場為準。",
+            "images": [
+                {
+                    "path": "images/products/20260816-02/01.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－葡萄牙原裝進口質感選品"
+                },
+                {
+                    "path": "images/products/20260816-02/02.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－植物精粹調配與紅蘋果香氣"
+                },
+                {
+                    "path": "images/products/20260816-02/03.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－提升日常私密護理質感體驗"
+                },
+                {
+                    "path": "images/products/20260816-02/04.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－溫熱觸感帶來豐富細緻感受"
+                },
+                {
+                    "path": "images/products/20260816-02/05.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－添加丁香薄荷油與維生素E"
+                },
+                {
+                    "path": "images/products/20260816-02/06.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－通過多項測試品質安心可靠"
+                },
+                {
+                    "path": "images/products/20260816-02/07.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－滴管設計方便取用且易水洗"
+                },
+                {
+                    "path": "images/products/20260816-02/08.png",
+                    "alt": "葡萄牙Orgie．小紅瓶2代 蜜豆快感熱感增強液－商品資訊總覽"
+                }
+            ]
+        },
+        {
             "updatedLabel": "2026-08-15｜新品展示",
             "name": "請填寫商品名稱。",
             "description": "請填寫商品特色與已確認的資訊。",
@@ -75,42 +115,6 @@ window.PRODUCT_CATALOG = {
                 {
                     "path": "images/products/20260814-01/08.png",
                     "alt": "玻尿酸潤滑液－商品資訊總覽"
-                }
-            ]
-        },
-        {
-            "updatedLabel": "2026-08-14｜首次商品展示",
-            "name": "KISSTOY Doris",
-            "description": "門市商品外觀展示，圖片僅供辨識與參考。",
-            "footnote": "實際庫存、售價與包裝版本以門市現場為準。",
-            "images": [
-                {
-                    "path": "images/products/kisstoy-doris/kisstoy-doris-main.jpg",
-                    "alt": "KISSTOY Doris 商品與外盒外觀"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/kisstoy-doris-package.jpg",
-                    "alt": "KISSTOY Doris 外盒包裝外觀"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/imgi_39_6def66484c644494bfe5891b4e0f676c.jpg",
-                    "alt": "KISSTOY Doris 商品功能示意"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/imgi_42_848b3db8a28e4fcea8c3491c97948eed.jpg",
-                    "alt": "KISSTOY Doris 遙控器與商品外觀"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/imgi_43_ddf704f1e132478494f9262ff29c1076.jpg",
-                    "alt": "KISSTOY Doris 操作強度示意"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/imgi_45_94ea3d460766420fb002eb26c2e57a04.jpg",
-                    "alt": "KISSTOY Doris 遙控器與商品展示"
-                },
-                {
-                    "path": "images/products/kisstoy-doris/imgi_50_ce3a47583ed240c2a673c6d2f73bb4ba.jpg",
-                    "alt": "KISSTOY Doris 商品與遙控器外觀"
                 }
             ]
         }
